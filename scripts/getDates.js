9542ea718c17e3356fbd785e8ab8580a6b6f4e38
josephwilms7 <josephwilmsdev@gmail.com>
2024-11-14 20:40:40 -0700

    So Far

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,11 @@ year.innerHTML = today.getFullYear();
 const lastModified = document.getElementById("lastModified");
 
 lastModified.innerHTML = document.lastModified;
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+// Toggle the 'active' class on click
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
