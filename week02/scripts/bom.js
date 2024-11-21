2af31d283afd30c8010bef855311ab791e2f31e2
josephwilms7 <josephwilmsdev@gmail.com>
2024-11-21 16:37:07 -0700

    Fixes for Week 2 and Week 3 ASNs Part 1 of 2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,22 +1,24 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#submitButton');
-const list = document.querySelector('#list');
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('#submitButton');
+    const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {
-    if (input.value != '') {
-        const li = document.createElement("li");
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
+    button.addEventListener('click', function() {
+        if (input.value != '') {
+            const li = document.createElement("li");
+            const deleteButton = document.createElement('button');
+            li.textContent = input.value;
+            deleteButton.textContent = '❌';
+            li.append(deleteButton);
+            list.append(li);
 
-        deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
+            deleteButton.addEventListener('click', function () {
+                list.removeChild(li);
+                input.focus();
+            });
+
             input.focus();
-        })
-
-        input.focus();
-        input.value = '';
-    }
+            input.value = '';
+        }
+    });
 });
