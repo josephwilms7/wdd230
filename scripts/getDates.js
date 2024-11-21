df79ea3ad50eecd10f8076e3bd25c5ac9a162e70
josephwilms7 <josephwilmsdev@gmail.com>
2024-11-21 13:04:46 -0700

    Week 3 Team ASN

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -11,7 +11,20 @@ lastModified.innerHTML = document.lastModified;
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
-// Toggle the 'active' class on click
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+const visitsDisplay = document.querySelector("#visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
