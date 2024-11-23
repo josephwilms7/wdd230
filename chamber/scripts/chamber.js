feeec58dbc721ac898bfd41207027026c757784f
josephwilms7 <josephwilmsdev@gmail.com>
2024-11-23 15:10:10 -0700

    Week 3 ASN Complete

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -6,3 +6,20 @@ function toggleMenu() {
 const lastModified = document.getElementById("lastModified");
 
 lastModified.innerHTML = document.lastModified;
+
+
+// Store the last visit date in localStorage
+const lastVisit = localStorage.getItem('lastVisit');
+const currentDate = Date.now();
+
+if (lastVisit) {
+    const timeDifference = currentDate - lastVisit;
+    const days = Math.floor(timeDifference / (1000 * 3600 * 24));
+    const visitMessage = days < 1 ? 'Back so soon! Awesome!' : `You last visited ${days} ${days === 1 ? 'day' : 'days'} ago.`;
+    document.getElementById('visit-message').innerText = visitMessage;
+} else {
+    document.getElementById('visit-message').innerText = 'Welcome! Let us know if you have any questions.';
+}
+
+// Save the current visit date
+localStorage.setItem('lastVisit', currentDate);
