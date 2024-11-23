64bd695508762ec7008f45cb717a57d8f07a6fd3
josephwilms7 <josephwilmsdev@gmail.com>
2024-11-23 14:36:47 -0700

    Fix for Week 3

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const menu = document.getElementById('nav-menu');
     menu.classList.toggle('active');
 }
+
+const lastModified = document.getElementById("lastModified");
+
+lastModified.innerHTML = document.lastModified;
